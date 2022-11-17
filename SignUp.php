fe594772ff4b4a21b77d5f67c13cb06bea426174
Elanaly <69584940+Elanaly@users.noreply.github.com>
2022-11-17 13:53:28 +0900

    不具合直し

--- SignUp.php
+++ SignUp.php
@@ -4,8 +4,8 @@
 session_start();
 
 $db['host'] = "localhost";  // DBサーバのURL
-$db['user'] = "hogeUser";  // ユーザー名
-$db['pass'] = "hogehoge";  // ユーザー名のパスワード
+$db['user'] = "eventuser";  // ユーザー名
+$db['pass'] = "omrn2022";  // ユーザー名のパスワード
 $db['dbname'] = "event_intro";  // データベース名
 
 // エラーメッセージ、登録完了メッセージの初期化
@@ -17,6 +17,8 @@ if (isset($_POST["signUp"])) {
     // 1. ユーザIDの入力チェック
     if (empty($_POST["username"])) {  // 値が空のとき
         $errorMessage = 'ユーザーIDが未入力です。';
+    } else if (empty($_POST["mailaddress"])) {
+        $errorMessage = 'メールアドレスが未入力です。';
     } else if (empty($_POST["password"])) {
         $errorMessage = 'パスワードが未入力です。';
     } else if (empty($_POST["password2"])) {
@@ -26,6 +28,7 @@ if (isset($_POST["signUp"])) {
     if (!empty($_POST["username"]) && !empty($_POST["password"]) && !empty($_POST["password2"]) && $_POST["password"] === $_POST["password2"]) {
         // 入力したユーザIDとパスワードを格納
         $username = $_POST["username"];
+        $mailaddress = $_POST["mailaddress"];
         $password = $_POST["password"];
 
         // 2. ユーザIDとパスワードが入力されていたら認証する
@@ -35,11 +38,11 @@ if (isset($_POST["signUp"])) {
         try {
             $pdo = new PDO($dsn, $db['user'], $db['pass'], array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
 
-            $stmt = $pdo->prepare("INSERT INTO userData(name, password) VALUES (?, ?)");
+            $stmt = $pdo->prepare("INSERT INTO users(username, mailaddress, password) VALUES (?, ?, ?)");
 
-            $stmt->execute(array($username, password_hash($password, PASSWORD_DEFAULT)));  // パスワードのハッシュ化
+            $stmt->execute(array($username, $mailaddress, password_hash($password, PASSWORD_DEFAULT)));  // パスワードのハッシュ化
             $userid = $pdo->lastinsertid();
-            $signUpMessage = '登録が完了しました。あなたは '. $userid. ' 番目に登録しました(^^♪パスワードは '. $password. ' です。ユーザー名とパスワードを忘れないようにしてください';  // ログイン時に使用するIDとパスワード
+            $signUpMessage = '登録が完了しました。';  // ログイン時に使用するIDとパスワード
         } catch (PDOException $e) {
             $errorMessage = 'データベースエラー';
           
@@ -49,7 +52,7 @@ if (isset($_POST["signUp"])) {
     }
 }
 ?>
-<body bgcolor="lightsalmon"//背景をオレンジにしてる
+<body bgcolor="white"//背景をオレンジにしてる
 <!doctype html>
 <html>
     <head>
@@ -65,6 +68,8 @@ if (isset($_POST["signUp"])) {
                 <div><font color="#0000ff"><?php echo htmlspecialchars($signUpMessage, ENT_QUOTES); ?></font></div>
                 <label for="username">ユーザー名</label><input type="text" id="username" name="username" placeholder="ユーザー名を入力" value="<?php if (!empty($_POST["username"])) {echo htmlspecialchars($_POST["username"], ENT_QUOTES);} ?>">
                 <br>
+                <label for="mailaddress">メールアドレス</label><input type="text" id="mailaddress" name="mailaddress" value="" placeholder="メールアドレスを入力">
+                <br>
                 <label for="password">パスワード</label><input type="password" id="password" name="password" value="" placeholder="パスワードを入力">
                 <br>
                 <label for="password2">パスワード(確認用)</label><input type="password" id="password2" name="password2" value="" placeholder="再度パスワードを入力">
@@ -74,7 +79,7 @@ if (isset($_POST["signUp"])) {
         </form>
         <br>
         <form action="Login.php">
-            <input type="submit" value="戻る">
+            <input type="submit" value="ログイン">
         </form>
     </body>
 </html>
