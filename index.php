8dc62706dbdf0ff272e662fef1eefa986fbd2bca
Elanaly <69584940+Elanaly@users.noreply.github.com>
2022-12-30 08:59:12 +0900

    リンク追加

--- index.php
+++ index.php
@@ -20,10 +20,10 @@ session_start();
     </h1>
     <nav class="pc-nav">
         <ul>
-            <li><a href="#">ホーム</a></li>
-            <li><a href="#">参加</a></li>
-            <li><a href="#">募集</a></li>
-            <li><a href="#">マイページ</a></li>
+            <li><a href="index.php">ホーム</a></li>
+            <li><a href="searchEvent.php">検索</a></li>
+            <li><a href="event_createForm.html">募集</a></li>
+            <li><a href="mypage.php">マイページ</a></li>
         </ul>
     </nav>
 </header>
@@ -31,11 +31,11 @@ session_start();
 <h2>サイトの紹介</h2>
 <p>ああああああああああ</p>    
 <div class="pad2"></div>
-<a href="" class="btn btn--orange btn-c">
+<a href="Login.php" class="btn btn--orange btn-c">
   <i class="fa far fa-envelope"></i>ログイン
 </a>
 <div class="pad3"></div>
-<a href="" class="btn btn--orange btn-c">
+<a href="SignUp.php" class="btn btn--orange btn-c">
   <i class="fa far fa-envelope"></i>会員登録
 </a>
 <br><br><br><br>
@@ -48,19 +48,23 @@ session_start();
     <th class='cap'>募集人数</th>
     <th class='key'>タグ</th>
   </tr>
+  
 <?php
 $dsn ='mysql:host=localhost;dbname=event_intro';
 $username ='root';
 try{
+  //期限切れは表示させない
   $dbh= new PDO($dsn,$username);
   $sql='select * from events';
   $sth =$dbh->prepare($sql);
   $sth->execute();
  $result = $sth->fetchAll();
+ 
+ 
 foreach ($result as $row) {
         $detail =$row['eventdetail'];
         $detail=insertStr($detail,"<br>",30);
-        echo "<tr><td class='title'>".$row['eventname']."</td>";
+        echo "<tr><td class='title'><a href='view.php?commentPage=1&id=".$row['id']."'>".$row['eventname']."</a></td>";
         echo "<td class='detail'>";
         echo $detail;
         echo "</td>";
