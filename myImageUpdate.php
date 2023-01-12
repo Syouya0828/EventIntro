d420e62bbe3e1c6a93917781e72ce1dd972d459d
a <st142005@m01.kyoto-kcg.ac.jp>
2023-01-12 11:38:22 +0900

    アイコン変更機能　firstcommit

---
+++ myImageUpdate.php
@@ -0,0 +1,28 @@
+<?php
+require_once('functions.php');
+$id = $_POST['id'];
+$username = htmlspecialchars($_POST['username']);
+$image = file_get_contents($_FILES['image']['tmp_name']);
+$image_type = $_FILES['image']['type'];
+$dbh = connectDB();
+if(!empty($_FILES['image']['name'])){
+    $dbh->exec("USE event_intro");
+    $sql = "UPDATE users SET username=:username, image=:image, image_type=:image_type WHERE id = '" .$id."'";
+    $prepare = $dbh->prepare($sql);
+    $prepare -> bindValue(':username', $username, PDO::PARAM_STR);
+    $prepare -> bindValue(':image', $image, PDO::PARAM_STR);
+    $prepare -> bindValue(':image_type', $image_type, PDO::PARAM_STR);
+    $prepare->execute();
+    $dbh = NULL;
+}else{
+    $dbh->exec("USE event_intro");
+    $sql = "UPDATE users SET username=:username WHERE id = '" .$id."'";
+    $prepare = $dbh->prepare($sql);
+    $prepare -> bindValue(':username', $username, PDO::PARAM_STR);
+    $prepare->execute();
+    $dbh = NULL;
+}
+header('Location: myImage.php');
+exit();
+
+?>
